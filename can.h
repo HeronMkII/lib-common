1de09679af45c4aea69e6ac9b54d72fc4f30e180
Ali Haydaroglu <ali.haydaroglu@mail.utoronto.ca>
2017-09-09 17:55:50 -0400

    syntax fix

--- can.h
+++ can.h
@@ -1,7 +1,7 @@
 #include "can_lib.h"
 
 void can_send_message(uint8_t data[], uint8_t size, uint16_t id);
-void init_rx_mob(* mob, uint8_t recieved_data[], uint8_t size, uint16_t id);
+void init_rx_mob(st_cmd_t* mob, uint8_t recieved_data[], uint8_t size, uint16_t id);
 void init_rx_interrupts(st_cmd_t mob);
 
 st_cmd_t rx_mob;
