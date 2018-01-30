bb0dcc783bbfffb91dddafebb3a04bdae2420322
J. W. Sheridan <james.sheridan@outlook.com>
2018-01-30 18:42:17 -0500

    Renamed queue library to stack
    * Created a proper queue library
    * Removed unnecessary code

--- include/queue/queue.h
+++ include/queue/queue.h
@@ -1,23 +1,25 @@
 /*
-    AUTHORS: Siddharth Mahendraker, Shimi Smith
+    AUTHORS: J. W. Sheridan, Siddharth Mahendraker, Shimi Smith
 */
 
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
 
-#define MAX_SIZE 50
-#define DATA_SIZE 8
+#define MAX_QUEUE_SIZE 50
+#define QUEUE_DATA_SIZE 8
 
 typedef struct {
-	uint8_t size;
-    uint8_t index;
-    uint8_t content[MAX_SIZE][DATA_SIZE];
+	uint8_t head;
+	uint8_t tail;
+ 	uint8_t size;
+	uint8_t index;
+  	uint8_t content[MAX_QUEUE_SIZE][QUEUE_DATA_SIZE];
 } queue_t;
 
 void init_queue(queue_t*);
-uint8_t is_full(queue_t*);
-uint8_t is_empty(queue_t*);
-
+uint8_t queue_full(queue_t*);
+uint8_t queue_empty(queue_t*);
 uint8_t enqueue(queue_t*, uint8_t*);
+uint8_t encircle(queue_t*, uint8_t*);
 uint8_t dequeue(queue_t*, uint8_t*);
