8b4337d8bb112e82ff65a51c96af0c14abb17781
J. W. Sheridan <james.sheridan@outlook.com>
2018-02-03 13:25:58 -0500

    new file stack.h inserted, variable names fixed in stack.h

--- include/stack/stack.h
+++ include/stack/stack.h
@@ -1,23 +1,20 @@
 /*
-    AUTHORS: Siddharth Mahendraker, Shimi Smith
+    AUTHORS: Siddharth Mahendraker, Shimi Smith, J. W. Sheridan
 */
-
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
-
 #define MAX_STACK_SIZE 50
 #define STACK_DATA_SIZE 8
 
 typedef struct {
 	uint8_t size;
     uint8_t index;
-    uint8_t content[MAX_SIZE][DATA_SIZE];
+    uint8_t content[MAX_STACK_SIZE][STACK_DATA_SIZE];
 } stack_t;
 
 void init_stack(stack_t*);
 uint8_t stack_full(stack_t*);
 uint8_t stack_empty(stack_t*);
-
 uint8_t enstack(stack_t*, uint8_t*);
 uint8_t destack(stack_t*, uint8_t*);
