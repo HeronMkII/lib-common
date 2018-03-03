43ade52e8284bd00c080e790c858c8bd28c5e6e7
Bruno Almeida <bruno.fa.almeida@gmail.com>
2018-03-03 16:30:33 -0500

    Update OBC-PAY CAN transmission protocol with standardized format
    - One CAN transmission per one sensor measurement
    - Sensor measurements are always 3 bytes, zero padded if necessary

--- include/can/packets.h
+++ include/can/packets.h
@@ -1,11 +1,16 @@
 /* OBC to PAY CAN packet transmission format */
 
 /*
-Each CAN transmission has 8 bytes of data.
-The first 2 bytes have the packet command and field (defining the data transmitted),
-followed by 6 bytes of the actual data.
+Each CAN transmission can have up to 8 bytes of data.
+
+The first 2 bytes have the packet and field (defining the data transmitted),
+followed by up to 6 bytes of the actual data.
+
 The 2 byte identification is sent both when the request is sent from OBC to PAY
-and when the response is sent from PAY to OBC, in order to identify the field in the overall packet.
+and when the response is sent from PAY to OBC, in order to identify the field in the packet.
+
+Currently, all sensor readings are transmitted and stored as 3 bytes values,
+so currently each transmission is 5 bytes.
 */
 
 
@@ -17,26 +22,27 @@ and when the response is sent from PAY to OBC, in order to identify the field in
 
 
 /*
-Byte 0: packet command
+Byte 0: packet
 (housekeeping or science data)
+
 TODO - is the "REQ" indicating that it will start requesting data (so prepare),
 and the "SENSOR_REQ" requesting data for a specific sensor?
 */
 
 // PAY COMMAND IDS
 #define PAY_HK_REQ			0b10000001
-#define PAY_HK_SENSOR_REQ	0b10000010
+// #define PAY_HK_SENSOR_REQ	0b10000010
 #define PAY_SCI_REQ			0b10000011
-#define PAY_SCI_SENSOR_REQ	0b10000100
+// #define PAY_SCI_SENSOR_REQ	0b10000100
 
 
 /*
 Byte 1: field
-(within the entire packet)
+(within the packet)
 Indicates which sensor to poll
 */
 
-// TODO - should the defined macros start at index 0?
+// TODO - should the defined macros start at 0?
 
 // PAY SENSOR IDS
 // For housekeeping data
@@ -49,43 +55,50 @@ Indicates which sensor to poll
 
 /*
 For science data
-Returns the OD and FL measurements in a single field
-Well numbering starts at 0 and counts up to 32
+OD and FL measurements are in separate fields
+
+TODO - standardize well numbering
+
+Bit 0: Always 0
+Bit 1: OD (0) or FL (1)
+Bits 2-7: Well index (0-32)
 */
 // #define PAY_WELL_OD_1		0b01000000
 // #define PAY_WELL_FL_1		0b11000000
 // #define PAY_WELL_OD_33		0b01100000
 // #define PAY_WELL_FL_33		0b11100000
-#define PAY_WELL_BASE       0b01000000
+#define PAY_WELL_OD_BASE        0b00000000
+#define PAY_WELL_FL_BASE        0b01000000
 /*
 To specify the well number, add the zero-based index of the well to this value, e.g.
-Well 1 (first): 0b01000000
-Well 11:        0b01001011
-Well 33 (last): 0b01100000
+Well 1 (first), OD: 0b00000000
+Well 11, FL:        0b01001011
+Well 33 (last), FL: 0b01100000
 */
 
 
 
 
 /*
-6 Bytes: Data
-Either housekeeping or science data
+Up to 6 bytes: data (currently always 3 bytes)
+For one particular sensor measurement
 */
 
 /*
 1. Temperature
-Bytes 2-3 (2 bytes) - 14 bit sensor reading
+14 bit sensor reading -> zero padded
+(data placed into 14 least significant bits, aligned to the right of the 3 bytes)
 */
 
 /*
 2. Pressure
-Bytes 2-5 (4 bytes)
-// TODO - is it a 32 bit sensor reading?
+24 bit sensor reading
 */
 
 /*
 3. Humidity
-Bytes 2-3 (2 bytes) - 14 bit sensor reading
+14 bit sensor reading -> zero padded
+(data placed into 14 least significant bits, aligned to the right of the 3 bytes)
 */
 
 /*
@@ -94,7 +107,11 @@ TODO - is it the same as normal temperature?
 */
 
 /*
-5. Well (OD and FL)
-Bytes 2-4 (3 bytes) - optical density (24 bit ADC reading)
-Bytes 5-7 (3 bytes) - fluorescence (24 bit ADC reading)
+5. Well OD (optical density)
+24 bit ADC reading
+*/
+
+/*
+6. Well FL (fluorescence)
+24 bit ADC reading
 */
