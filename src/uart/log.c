2635c2150e8a21bb4c62899dde60aaff798e2fb9
Siddharth Mahendraker <siddharth.mahen@gmail.com>
2018-03-20 20:45:32 -0400

    Rename print buffer
    * Also make print buffer smaller to save space.

--- src/uart/log.c
+++ src/uart/log.c
@@ -1,16 +1,16 @@
 #include <uart/log.h>
 
 // TODO: should this be volatile
-uint8_t buf[LOG_BUFFER_SIZE];
+uint8_t log_buf[LOG_BUF_SIZE];
 
 // UART must be initialized before calling print
 inline int print(char *str, ...) {
     va_list ptr;
     va_start(ptr, str);
 
-    int ret = vsprintf((char *)buf, str, ptr);
+    int ret = vsprintf((char *)log_buf, str, ptr);
     va_end(ptr);
 
-    send_uart(buf, strlen((char*)buf));
+    send_uart(log_buf, strlen((char*)log_buf));
     return ret;
 }
