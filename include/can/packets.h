53d586d4dc34f7587c48227d0bcac78ce7a229f3
Bruno Almeida <bruno.fa.almeida@gmail.com>
2018-03-24 18:38:06 -0400

    Clarify OBC-PAY CAN protocol documentation, update constants

--- include/can/packets.h
+++ include/can/packets.h
@@ -1,115 +1,100 @@
-/* OBC to PAY CAN packet transmission format */
-
 /*
+OBC-PAY CAN packet transmission format
+
+
 Each CAN transmission can have up to 8 bytes of data.
+For now, can transmit all 8 bytes for simplicity and ignore the unused bytes.
 
-The first 2 bytes have the packet and field (defining the data transmitted),
-followed by up to 6 bytes of the actual data.
 
-The 2 byte identification is sent both when the request is sent from OBC to PAY
-and when the response is sent from PAY to OBC, in order to identify the field in the packet.
+Byte 0: Message Type (broad classification)
 
-Currently, all sensor readings are transmitted and stored as 3 bytes values,
-so currently each transmission is 5 bytes.
+Byte 1: Field Number (specifics for type of message)
+- e.g. which sensor to poll
+- e.g. which heater to control
+
+Bytes 2-4: Data (for response only, when PAY sends a message to OBC)
+- If data is smaller than 24 bits, it is right-aligned to the least significant bits
+
+
+Bytes 0-1 are sent both when the request is sent from OBC to PAY
+and when the response is sent from PAY to OBC.
+This is to identify which data is being transmitted.
+
+TODO - maybe add ACK bit when PAY responds to OBC, indicating the message was valid or not?
 */
 
 
 
 
+// Byte 0: Message Type
+
+// TODO - should numbering start at 0? should they remain one-hot?
+// Housekeeping data (environmental sensors, e.g. temperature, humidity, pressure)
+#define PAY_HK_REQ          0b00000001
+// Science data (optical sensors)
+#define PAY_SCI_REQ         0b00000010
+// Heater control (TODO)
+#define PAY_HEATER_REQ      0b00000100
+
+
+
+
+// Byte 1: Field Number
+
+
 /*
-2 Bytes: Identification
+Housekeeping
+TODO - should numbering start at 0? one-hot?
 */
+#define PAY_TEMP_1          0b00000001
+#define PAY_PRES_1          0b00000010
+#define PAY_HUMID_1         0b00000100
+#define PAY_MF_TEMP_1       0b00001000
+#define PAY_MF_TEMP_2       0b00010000
+#define PAY_MF_TEMP_3       0b00100000
 
 
 /*
-Byte 0: packet
-(housekeeping or science data)
-*/
-
-// PAY COMMAND IDS
-#define PAY_HK_REQ          0b10000001
-#define PAY_SCI_REQ         0b10000011
-
-
-/*
-Byte 1: field
-(within the packet)
-Indicates which sensor to poll
-*/
-
-// TODO - should the defined macros start at 0?
-
-// PAY SENSOR IDS
-// For housekeeping data
-#define PAY_TEMP_1          0b00000000
-#define PAY_PRES_1          0b00000001
-#define PAY_HUMID_1         0b00000010
-#define PAY_MF_TEMP_1       0b00000011
-#define PAY_MF_TEMP_2       0b00000100
-#define PAY_MF_TEMP_3       0b00000101
-
-/*
-For science data
-OD and FL measurements are in separate fields
-
+Science
 TODO - standardize well numbering
-TODO - implement reading/triplicate number in transmission (1, 2, or 3)
-
-Bit 0: Always 0
-Bit 1: OD (0) or FL (1)
-Bits 2-7: Well index (0-32)
+TODO - reading/triplicate number (1-3)
+TODO - OD/FL differentiation
 */
-// #define PAY_WELL_OD_1    0b01000000
-// #define PAY_WELL_FL_1    0b11000000
-// #define PAY_WELL_OD_33   0b01100000
-// #define PAY_WELL_FL_33   0b11100000
-#define PAY_WELL_OD_BASE    0b00000000
-#define PAY_WELL_FL_BASE    0b01000000
-/*
-To specify the well number, add the zero-based index of the well to this value, e.g.
-Well 1 (first), OD: 0b00000000
-Well 11, FL:        0b01001011
-Well 33 (last), FL: 0b01100000
-*/
+// These are just for testing with the 2 optical sensors on the PAY sensor PCB
+#define PAY_SCI_TEMD        0b00000001
+#define PAY_SCI_SFH         0b00000010
 
 
+
+
+// Bytes 2-4: Data
 
 
 /*
-Up to 6 bytes: data (currently always 3 bytes)
-For one particular sensor measurement
-*/
+Different possible types of data:
 
-/*
+
 1. Temperature
-14 bit sensor reading -> zero padded
-(data placed into 14 least significant bits, aligned to the right of the 3 bytes)
-*/
+- 14 bit sensor reading -> zero padded
+- Data placed into 14 least significant bits, aligned to the right of the 3 bytes
 
-/*
+
 2. Pressure
-24 bit sensor reading
-*/
+- 24 bit sensor reading
+- TODO - figure out how to account for temperature corrections
 
-/*
+
 3. Humidity
-14 bit sensor reading -> zero padded
-(data placed into 14 least significant bits, aligned to the right of the 3 bytes)
-*/
+- 14 bit sensor reading -> zero padded
+- Data placed into 14 least significant bits, aligned to the right of the 3 bytes
 
-/*
+
 4. MF Temperature (thermistors)
-12 bit ADC reading -> zero padded
-(data placed into 12 least significant bits, aligned to the right of the 3 bytes)
-TODO - confirm the number of thermistors
-*/
+- TODO
+- 12 bit ADC reading -> zero padded
+- Data placed into 12 least significant bits, aligned to the right of the 3 bytes
 
-/*
-5. Well OD (optical density)
-24 bit ADC reading
-*/
 
-/*
-6. Well FL (fluorescence)
-24 bit ADC reading
+5. Well FL (fluorescence)
+- 24 bit ADC reading
 */
