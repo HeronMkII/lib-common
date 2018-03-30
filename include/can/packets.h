d21ae90de579c3d870c9f18e43d514d350df6c5c
Bruno Almeida <bruno.fa.almeida@gmail.com>
2018-03-29 21:55:21 -0400

    Clarify documentation wording in packets.h

--- include/can/packets.h
+++ include/can/packets.h
@@ -3,12 +3,12 @@ OBC-PAY CAN packet transmission format
 
 
 Each CAN transmission can have up to 8 bytes of data.
-For now, can transmit all 8 bytes for simplicity and ignore the unused bytes.
+For now, transmit all 8 bytes for simplicity and ignore the unused bytes.
 
 
 Byte 0: Message Type (broad classification)
 
-Byte 1: Field Number (specifics for type of message)
+Byte 1: Field Number (specifics for message type)
 - e.g. which sensor to poll
 - e.g. which heater to control
 
@@ -16,11 +16,14 @@ Bytes 2-4: Data (for response only, when PAY sends a message to OBC)
 - If data is smaller than 24 bits, it is right-aligned to the least significant bits
 
 
-Bytes 0-1 are sent both when the request is sent from OBC to PAY
-and when the response is sent from PAY to OBC.
-This is to identify which data is being transmitted.
+Bytes 0-1 are sent both when the request is sent from OBC to PAY and when the
+response is sent from PAY to OBC. This is to identify which data is being
+transmitted.
 
 TODO - maybe add ACK bit when PAY responds to OBC, indicating the message was valid or not?
+TODO - standardize numbering system for constants
+     - should numbering start at 0?
+     - one-hot encoding?
 */
 
 
@@ -28,7 +31,7 @@ TODO - maybe add ACK bit when PAY responds to OBC, indicating the message was va
 
 // Byte 0: Message Type
 
-// TODO - should numbering start at 0? should they remain one-hot?
+
 // Housekeeping data (environmental sensors, e.g. temperature, humidity, pressure)
 #define PAY_HK_REQ          0b00000001
 // Science data (optical sensors)
@@ -41,11 +44,7 @@ TODO - maybe add ACK bit when PAY responds to OBC, indicating the message was va
 
 // Byte 1: Field Number
 
-
-/*
-Housekeeping
-TODO - should numbering start at 0? one-hot?
-*/
+// Housekeeping
 #define PAY_TEMP_1          0b00000001
 #define PAY_PRES_1          0b00000010
 #define PAY_HUMID_1         0b00000100
@@ -53,12 +52,11 @@ TODO - should numbering start at 0? one-hot?
 #define PAY_MF_TEMP_2       0b00010000
 #define PAY_MF_TEMP_3       0b00100000
 
-
+// Science
 /*
-Science
-TODO - standardize well numbering
+TODO - create well numbering system
 TODO - reading/triplicate number (1-3)
-TODO - OD/FL differentiation
+TODO - OD/FL
 */
 // These are just for testing with the 2 optical sensors on the PAY sensor PCB
 #define PAY_SCI_TEMD        0b00000001
@@ -69,32 +67,25 @@ TODO - OD/FL differentiation
 
 // Bytes 2-4: Data
 
-
 /*
-Different possible types of data:
-
+Possible data types:
 
 1. Temperature
 - 14 bit sensor reading -> zero padded
-- Data placed into 14 least significant bits, aligned to the right of the 3 bytes
+- Data in the 14 least significant bits, aligned to the right of the 3 bytes
 
-
-2. Pressure
+2. Pressure - TODO
 - 24 bit sensor reading
 - TODO - figure out how to account for temperature corrections
 
-
 3. Humidity
 - 14 bit sensor reading -> zero padded
-- Data placed into 14 least significant bits, aligned to the right of the 3 bytes
+- Data in the 14 least significant bits, aligned to the right of the 3 bytes
 
-
-4. MF Temperature (thermistors)
-- TODO
+4. MF Temperature (thermistors) - TODO
 - 12 bit ADC reading -> zero padded
-- Data placed into 12 least significant bits, aligned to the right of the 3 bytes
+- Data in the 12 least significant bits, aligned to the right of the 3 bytes
 
-
-5. Well FL (fluorescence)
+5. Well FL (fluorescence) or OD (optical density)
 - 24 bit ADC reading
 */
