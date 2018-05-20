9cd44447c4eed2cd6995a855ddca22d23d0d5894
Bruno Almeida <bruno.fa.almeida@gmail.com>
2018-05-19 23:31:35 -0400

    Revert "Merge branch 'queue-fix' into master"
    This reverts commit 010e7c85e16a9442b2f1868fe7de5ddb594366a4, reversing
    changes made to da89ced1ab5b6d8b9de82ce2f2aa49957db6ca63.

--- examples/queue_echo/queue_echo.c
+++ examples/queue_echo/queue_echo.c
@@ -2,64 +2,29 @@
 #include <uart/log.h>
 #include <queue/queue.h>
 
-void print_queue_data(queue_t* queue){
-    print("Size: %d\n", queue->size);
-    print("Head: %d\n", queue->head);
-    print("Tail: %d\n\n", queue->tail);
-}
-
 int main(void) {
     init_uart();
     print("UART initialized\n");
 
     queue_t queue;
     init_queue(&queue);
-    print("Queue initialized\n");
-    print_queue_data(&queue);
 
-    // Enqueue a bit of data
-    uint8_t fail = enqueue(&queue, (uint8_t*)"Hello!");
-    if (!fail) {
-        print("Enqueue successful\n");
-        print("Content: %s\n", queue.content[queue.head]);
-        print_queue_data(&queue);
-    }
-
-    // Dequeue a bit of data
-    uint8_t data[QUEUE_DATA_SIZE] = { 0 };
-    fail = dequeue(&queue, data);
-    if (!fail){
-        print("Dequeue successful\n");
-        print("Content: %s\n", data);
-        print_queue_data(&queue);
-    }
-
-    // Test the queue to maximum size
-    uint8_t enqueue_data[QUEUE_DATA_SIZE] = { 0 };
-    for (uint8_t i = 0; i < MAX_QUEUE_SIZE + 1; i++){
-        enqueue_data[0] = i;
-        fail = enqueue(&queue, enqueue_data);
-        if (!fail){
+    for (uint8_t i = 0; i < 10; i++) {
+        uint8_t fail = enqueue(&queue, (uint8_t*) "Hello!");
+        if (!fail) {
             print("Enqueue successful\n");
-            print("Content: %d\n", queue.content[i][0]);
-            print_queue_data(&queue);
-        } else {
-            print("Enqueue failed\n");
-            print("Content: %d\n", i);
-            print_queue_data(&queue);
+            print("Size: %d\n", queue.size);
+            print("Content: %s\n", queue.content[i]);
         }
     }
 
-    // Now dequeue the elements we added
-    for (uint8_t i = 0; i < MAX_QUEUE_SIZE + 1; i++){
+    while (!is_empty(&queue)) {
+        uint8_t data[8] = {0};
         uint8_t fail = dequeue(&queue, data);
-        if (!fail){
+        if (!fail) {
             print("Dequeue successful\n");
-            print("Content: %d\n", data[0]);
-            print_queue_data(&queue);
-        } else {
-            print("Dequeue failed\n");
-            print_queue_data(&queue);
+            print("Size: %d\n", queue.size);
+            print("Content: %s\n", data);
         }
     }
 }
--- src/queue/queue.c
+++ src/queue/queue.c
@@ -1,24 +1,18 @@
 /*
-    AUTHORS: J. W. Sheridan, Shimi Smith, Siddharth Mahendraker
+    AUTHORS: Shimi Smith, Siddharth Mahendraker
 
-    A queue implementation which does not allocate heap memory.
+    A queue implementation which does not allocate memory to the heap.
 */
 
 #include <queue/queue.h>
 
 void init_queue(queue_t* queue) {
     queue->size = 0;
-    queue->head = 0;
-    queue->tail = 0;
-    for (uint8_t i = 0; i < MAX_QUEUE_SIZE; i++){
-        for (uint8_t j = 0; j < QUEUE_DATA_SIZE; j++){
-            queue->content[i][j] = 0x00;
-        }
-    }
+    queue->index = 0;
 }
 
 uint8_t is_full(queue_t* queue) {
-    return (queue->size == MAX_QUEUE_SIZE);
+    return (queue->size == MAX_SIZE);
 }
 
 uint8_t is_empty(queue_t* queue) {
@@ -26,37 +20,29 @@ uint8_t is_empty(queue_t* queue) {
 }
 
 uint8_t enqueue(queue_t* queue, uint8_t* data) {
-    if (is_full(queue)) {
+    if(is_full(queue)) {
         return 1;
     } else {
-        uint8_t index = queue->tail;
-        for (uint8_t i = 0; i < QUEUE_DATA_SIZE; i++) {
+        uint8_t index = queue->index;
+        for (uint8_t i = 0; i < DATA_SIZE; i++) {
             (queue->content)[index][i] = data[i];
         }
+        queue->index+= 1;
         queue->size += 1;
-        if (!(queue->tail == MAX_QUEUE_SIZE-1)) {
-            queue->tail++;
-        } else {
-            queue->tail = 0;
-        }
         return 0;
     }
 }
 
 uint8_t dequeue(queue_t* queue, uint8_t* data) {
-    if (is_empty(queue)) {
+    if(is_empty(queue)) {
         return 1;
     } else {
-        uint8_t index = queue->head;
-        for (uint8_t i = 0; i < QUEUE_DATA_SIZE; i++) {
+        queue->index -= 1;
+        uint8_t index = queue->index;
+        for (uint8_t i = 0; i < DATA_SIZE; i++) {
             data[i] = (queue->content)[index][i];
         }
         queue->size -= 1;
-        if (!(queue->head == MAX_QUEUE_SIZE-1)) {
-            queue->head += 1;
-        } else {
-            queue->head = 0;
-        }
         return 0;
     }
 }
