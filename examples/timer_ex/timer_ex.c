1439410e7ffcaefcbe2f92cf6605658e12c22f2e
Brytni Richards <brytniri@gmail.com>
2018-05-30 18:51:08 -0400

    Added comments on functionality

--- examples/timer_ex/timer_ex.c
+++ examples/timer_ex/timer_ex.c
@@ -1,10 +1,14 @@
+/*
+Authors: Brytni Richards
+
+Tests timers by incrementing a global variable specific for both timers every minute
+*/
+
 #include <uart/uart.h>
 #include <uart/log.h>
 #include <timer/timer.h>
 #include <timer/timer2.h>
 #include <assert/assert.h>
-
-//Tests timers
 
 int count1 = 0;//current timer 1 iteration
 int count2 = 0;//current timer 2 iteration
@@ -22,7 +26,7 @@ int main() {
   init_uart();
   print("UART initialized\n");
 
-  init_timer(1, timer1_func);//increases count every 1 minutes
+  init_timer(1, timer1_func);//increases count every 1 minute
   init_timer2(1, timer2_func);
 
   while(1);
