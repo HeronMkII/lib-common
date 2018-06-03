e7e68496e014e0be9756de945bbc629223d1abde
Brytni Richards <brytniri@gmail.com>
2018-06-03 15:01:29 -0400

    Unified timer source and header files
    * Removed unnecessary initialization functions
    * Further style improvements

---
+++ examples/timer_print/timer_print.c
@@ -0,0 +1,34 @@
+/*
+Author: Brytni Richards
+
+Tests timers by incrementing a global variable specific for both timers every
+minute or 5 minutes
+*/
+
+#include <uart/uart.h>
+#include <uart/log.h>
+#include <timer/timer.h>
+
+int count1 = 0; //current timer 1 iteration
+int count2 = 0; //current timer 2 iteration
+
+void timer1_func() {
+    count1++;
+    print("Timer 1: %d\n", count1);
+}
+void timer2_func() {
+    count2++;
+    print("Timer 2: %d\n", count2);
+}
+
+int main() {
+    init_uart();
+    print("UART initialized\n");
+
+    init_timer_16bit(1, timer1_func);//increases count every 1 minute
+    init_timer_8bit(5, timer2_func);//increases count every 5 minutes
+    print("Timers started\n");
+
+    while (1) {};
+    return 0;
+}
