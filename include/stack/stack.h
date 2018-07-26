a30d6b413e53f24e6f4c3773c253ba4a44f218ee
Siddharth Mahendraker <siddharth.mahen@gmail.com>
2018-07-26 17:11:24 -0400

    Rewrote queue, stack internals
    * The size fields of both structs have been replaced by on-the-fly index
    computations.
    
    * The dequeue() and pop() functions now accept NULL as a second argument
    instead of an array of uint8_t's. This has the effect of throwing away
    the data contained in the dequeued/popped element. This is useful if you
    don't care about this data, or already know what it is.
    
    * The dequeue() and pop() functions now erase the memory contents of the
    dequeued/popped element.
    
    * Successful queue and stack operations now return 1 instead of 0. This
    makes for more idiomatic code.
    
    * Both the queue and stack is_full() and is_empty() functions have been
    renamed to queue_full(), stack_full() and queue_empty(), stack_empty(),
    respectively.
    
    * Improved the enqueue() and push() function signatures to communicate
    that the data passed in will not be modified.
    
    * Minor style fixes.

--- include/stack/stack.h
+++ include/stack/stack.h
@@ -6,20 +6,21 @@
 */
 
 #include <stdint.h>
+#include <stdlib.h> // for NULL
 
-#define MAX_STACK_SIZE 10
+#define MAX_STACK_SIZE 5
 #define STACK_DATA_SIZE 8
 
 typedef struct {
-    uint8_t size;
     uint8_t index;
     uint8_t content[MAX_STACK_SIZE][STACK_DATA_SIZE];
 } stack_t;
+// NOTE: index is always equal to the stack's size
 
 void init_stack(stack_t*);
-uint8_t is_full(stack_t*);
-uint8_t is_empty(stack_t*);
-uint8_t push(stack_t*, uint8_t*);
+uint8_t stack_full(stack_t*);
+uint8_t stack_empty(stack_t*);
+uint8_t push(stack_t*, const uint8_t*);
 uint8_t pop(stack_t*, uint8_t*);
 
-#endif
+#endif // STACK_H
--- src/queue/queue.c
+++ src/queue/queue.c
@@ -7,7 +7,6 @@
 #include <queue/queue.h>
 
 void init_queue(queue_t* queue) {
-    queue->size = 0;
     queue->head = 0;
     queue->tail = 0;
     for (uint8_t i = 0; i < MAX_QUEUE_SIZE; i++) {
@@ -17,46 +16,54 @@ void init_queue(queue_t* queue) {
     }
 }
 
-uint8_t is_full(queue_t* queue) {
-    return (queue->size == MAX_QUEUE_SIZE);
+uint8_t queue_full(queue_t* queue) {
+    return ((queue->tail - queue->head) == MAX_QUEUE_SIZE);
 }
 
-uint8_t is_empty(queue_t* queue) {
-    return (queue->size == 0);
+uint8_t queue_empty(queue_t* queue) {
+    return ((queue->tail - queue->head) == 0);
 }
 
-uint8_t enqueue(queue_t* queue, uint8_t* data) {
-    if (is_full(queue)) {
-        return 1;
+void shift_left(queue_t* queue) {
+    for (uint8_t i = queue->head; i < queue->tail; i++) {
+        for (uint8_t j = 0; j < QUEUE_DATA_SIZE; j++) {
+            queue->content[i - (queue->head)][j] = queue->content[i][j];
+            queue->content[i][j] = 0x00;
+        }
+    }
+
+    queue->tail = (queue->tail) - (queue->head);
+    queue->head = 0;
+}
+
+uint8_t enqueue(queue_t* queue, const uint8_t* data) {
+    if (queue_full(queue)) {
+        return 0;
     } else {
+        if (queue->tail == MAX_QUEUE_SIZE) {
+            shift_left(queue);
+        }
         uint8_t index = queue->tail;
         for (uint8_t i = 0; i < QUEUE_DATA_SIZE; i++) {
             (queue->content)[index][i] = data[i];
         }
-        queue->size += 1;
-        if (!(queue->tail == MAX_QUEUE_SIZE-1)) {
-            queue->tail++;
-        } else {
-            queue->tail = 0;
-        }
-        return 0;
+        queue->tail += 1;
+        return 1;
     }
 }
 
 uint8_t dequeue(queue_t* queue, uint8_t* data) {
-    if (is_empty(queue)) {
-        return 1;
+    if (queue_empty(queue)) {
+        return 0;
     } else {
         uint8_t index = queue->head;
         for (uint8_t i = 0; i < QUEUE_DATA_SIZE; i++) {
-            data[i] = (queue->content)[index][i];
+            if (data != NULL) {
+                data[i] = (queue->content)[index][i];
+            }
+            (queue->content)[index][i] = 0x00;
         }
-        queue->size -= 1;
-        if (!(queue->head == MAX_QUEUE_SIZE-1)) {
-            queue->head += 1;
-        } else {
-            queue->head = 0;
-        }
-        return 0;
+        queue->head += 1;
+        return 1;
     }
 }
