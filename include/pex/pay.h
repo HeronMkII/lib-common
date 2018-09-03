2b6fff58dd136cab7933f7c84379be4f536683ae
Bruno Almeida <bruno.fa.almeida@gmail.com>
2018-09-02 21:01:17 -0400

    Fix PAY_ADDR_EPS -> PEX_ADDR_EPS

--- include/pex/pay.h
+++ include/pex/pay.h
@@ -1,6 +1,7 @@
 #ifndef PEX_PAY_H
 #define PEX_PAY_H
 
+// CS and RST pins on the PAY microcontroller
 #define PEX_CS_PIN_PAY      PD5
 #define PEX_CS_PORT_PAY     PORTD
 #define PEX_CS_DDR_PAY      DDRD
