b042b89849f9be29f537af3235babb7979edef30
Ambrose <ambrose.man@mail.utoronto.ca>
2019-02-02 13:42:51 -0500

    Complete Timer Review
    - completed documentation
    - changed confusing variable names
    - waiting to test...

--- include/timer/timer.h
+++ include/timer/timer.h
@@ -4,7 +4,7 @@
 /*
 Authors: Shimi Smith, Matthew Silverman
 
-Runs an 8-bit and 16-bit timer.
+Runs an 8-bit (Timer 0) and 16-bit (Timer 1) timer.
 
 For 8-bit timer, to compensate for the differences in 16-bit timer,
 I use a 16-bit int to store the number of interrupts required. timer_16bit cannot
@@ -21,17 +21,10 @@ https://www.newbiehack.com/TimersandCountersDefaultandBasicUsage.aspx
 #ifndef F_CPU
 #define F_CPU 8000000
 #endif
-
-#define PRESCALER 1024. //prescaler factor 
-
-
-#define T ((PRESCALER / F_CPU) * 1000.0)
-// timer clock period (ms)
-#define MAX_TIME_16BIT (T * 0xFF)//check - based on datasheet, it should be 0xFF
-// the maximum time the 16 bit timer can hold until it overflows (ms) p.127
-#define MAX_TIME_8BIT (T * 0xF) //check - based on datasheet, it should be 0xF
-// the maximum time the 8 bit timer can hold until it overflows (ms)
-
+#define PRESCALER 1024. //prescaler factor - divide clock by 1024
+#define PERIOD ((PRESCALER / F_CPU) * 1000.0) // timer clock period (ms)
+#define MAX_TIME_16BIT (T * 0xFF) //counter maximum (16-bit) p.127
+#define MAX_TIME_8BIT (T * 0xF) //counter maximum (8 bit) p.127
 #define ROUND 0.5  // used to round double to int
 
 typedef void(*cmd_fn_t)(void);
@@ -39,12 +32,12 @@ typedef void(*cmd_fn_t)(void);
 void init_timer_16bit(uint8_t minutes, cmd_fn_t cmd);
 void init_timer_8bit(uint8_t minutes, cmd_fn_t cmd);
 
-// This struct holds important variables for the 8-bit timer
+// This struct holds important variables for the 8-bit (and 16-bit) timer
 typedef struct {
   uint16_t ints;
     // the number of interrupts that will occur to achieve the desired time
-	uint16_t count;
-    // the value of the timer counter after the desired time has ellapsed
+	uint16_t remainder;
+    // remamining timer counter value after the desired time has ellapsed
 	cmd_fn_t cmd;
     // The command to run once the desired time has passed
 } timer_t;
--- src/timer/timer.c
+++ src/timer/timer.c
@@ -11,9 +11,9 @@ static timer_t timer8;
 
 void init_timer_16bit(uint8_t seconds, cmd_fn_t cmd) {
     uint32_t delay = seconds * 1000L;  // delay in ms
-    timer16.ints = ((delay / MAX_TIME_16BIT));
-    timer16.count = ((delay - (timer16.ints * MAX_TIME_16BIT)) / T) + ROUND;
-    timer16.cmd = cmd;
+    timer16.ints = ((delay / MAX_TIME_16BIT));//number of interrupts
+    timer16.remainder = ((delay - (timer16.ints * MAX_TIME_16BIT)) / PERIOD) + ROUND;//counter value
+    timer16.cmd = cmd; //command to run
 
     // set timer to CTC mode - using OCR1Au
     TCCR1A &= ~(_BV(WGM10) | _BV(WGM11));
@@ -37,9 +37,9 @@ void init_timer_16bit(uint8_t seconds, cmd_fn_t cmd) {
 
 void init_timer_8bit(uint8_t seconds, cmd_fn_t cmd) {
     uint32_t delay = seconds * 1000L;  // delay in ms
-    timer8.ints = ((delay / MAX_TIME_8BIT));
-    timer8.count = ((delay - (timer8.ints * MAX_TIME_8BIT)) / T) + ROUND;
-    timer8.cmd = cmd;
+    timer8.ints = ((delay / MAX_TIME_8BIT)); // number of interrupts
+    timer8.remainder = ((delay - (timer8.ints * MAX_TIME_8BIT)) / PERIOD) + ROUND; //counter value
+    timer8.cmd = cmd; //command to run
 
     // set timer to CTC mode - using OCR0A
     TCCR0A &= ~_BV(WGM00);
@@ -50,7 +50,7 @@ void init_timer_8bit(uint8_t seconds, cmd_fn_t cmd) {
     TCCR0B |= _BV(CS02) | _BV(CS00);
     TCCR0B &= ~_BV(CS01);
 
-    // disable use of Output compare pins so that they can be used as normal pins
+    // disable use of output compare pins so that they can be used as normal pins
     TCCR0A &= ~(_BV(COM0A1) | _BV(COM0A0) | _BV(COM0B1) | _BV(COM0B0));
 
     TCNT0 = 0;  // initialize 8 bit counter at 0
@@ -67,32 +67,32 @@ volatile uint32_t counter16 = 0;
 // Counts the number of interrupts that have occured for the 8 bit timer
 volatile uint32_t counter8 = 0;
 
-// This ISR occurs when TCNT1 is equal to OCR1A
+// This ISR occurs when TCNT1 is equal to OCR1A for a 16-bit timer
+// Timer 1 compare match A handler
 ISR(TIMER1_COMPA_vect) {
-    counter16 += 1;
+    counter16 += 1; //counting number of interrupts
     if (counter16 == timer16.ints) {
-        OCR1A = timer16.count;
-    } else if (counter16 >= timer16.ints + 1) {
+        OCR1A = timer16.remainder;
+    }
+    else if (counter16 >= timer16.ints + 1) {
         // the desired time has passed
-        counter16 = 0;
-        OCR1A = 0xFFFF;
+        counter16 = 0; // reset the number of interrupts to 0
+        OCR1A = 0xFFFF; //set compare value
         (timer16.cmd)();
     }
 }
 
-// This ISR occurs when TCNT0 is equal to OCR0A
-
+// This ISR occurs when TCNT0 is equal to OCR0A for a 8-bit timer
+// Timer 0 compare match A handler
 ISR(TIMER0_COMPA_vect) {
-    // TODO: Should this happen in an atomic block?
-    counter8 += 1;
+    counter8 += 1; //counting number of interrupts
     if (counter8 == timer8.ints) {
-        OCR0A = timer8.count;
-    } else if (counter8 >= timer8.ints + 1) {
+        OCR0A = timer8.remainder;
+    }
+    else if (counter8 >= timer8.ints + 1) {
         // the desired time has passed
-        counter8 = 0;
-        OCR0A = 0xFF;
+        counter8 = 0; // reset the number of interrupts to 0
+        OCR0A = 0xFF; //set compare value
         (timer8.cmd)();
     }
 }
-
-// TODO: write an ISR for 16 bit timer?
