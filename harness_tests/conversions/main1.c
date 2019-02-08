3213ad89ba8b12ea7def10ce462ab7e672b42725
R-Song <ryansong02@gmail.com>
2019-02-07 20:21:47 -0500

    Fixed warnings, typos, and floating point assertions in the conversions test

--- harness_tests/conversions/main1.c
+++ harness_tests/conversions/main1.c
@@ -4,89 +4,90 @@
 void adc_data_to_voltage_test(void) {
     uint16_t raw_data_0 = 0x0FFF;
     double voltage_0 = 2.5;
-    ASSERT_EQ(adc_data_to_voltage(raw_data), voltage_0);
+    ASSERT_FP_EQ(adc_raw_data_to_raw_vol(raw_data_0), voltage_0);
 
     uint16_t raw_data_1 = 0x0333;
     double voltage_1 = 0.5;
-    ASSERT_EQ(adc_data_to_voltage(raw_data_1), voltage_1);
+    ASSERT_FP_EQ(adc_raw_data_to_raw_vol(raw_data_1), voltage_1);
 }
 
 void adc_raw_vol_to_eps_vol_test(void) {
     double raw_voltage = 2.5;
-    ASSERT_EQ(adc_data_to_voltage(raw_voltage), 5.0);
+    ASSERT_FP_EQ(adc_raw_data_to_raw_vol(raw_voltage), 5.0);
 }
 
 void adc_raw_vol_to_eps_cur_test(void) {
     double raw_voltage = 5.3;
-    ASSERT_EQ(adc_data_to_voltage_to_current(raw_voltage), 2.0);
+    double current = adc_raw_vol_to_eps_cur(raw_voltage);
+    ASSERT_FP_EQ(current, 2.0);
 }
 
 void adc_raw_data_to_eps_vol_test(void) {
     uint16_t raw_data = 0x0FFF;
-    ASSERT_EQ(adc_raw_data_to_eps_vol(raw_data), 10.0);
+    ASSERT_FP_EQ(adc_raw_data_to_eps_vol(raw_data), 10.0);
 }
 
 void adc_raw_data_to_eps_cur_test(void) {
     uint16_t raw_data = 0x0FFF;
-    ASSERT_EQ(adc_raw_data_to_eps_cur(), 1.7);
+    ASSERT_FP_EQ(adc_raw_data_to_eps_cur(raw_data), 1.7);
 }
 
 void adc_raw_data_to_therm_temp_test(void) {
     // 0x375 -> 0.540 V -> 36.296 kohm -> -6.395 C
-    ASSERT_EQ(adc_raw_data_to_therm_temp(0x375), -6.395);
+    ASSERT_FP_EQ(adc_raw_data_to_therm_temp(0x375), -6.395);
 }
 
 void dac_raw_data_to_vol_test(void) {
     uint16_t raw_data = 0x3000;
-    ASSERT_EQ(dac_raw_data_to_vol(raw_data), 15.0);
+    ASSERT_FP_EQ(dac_raw_data_to_vol(raw_data), 15.0);
 }
 
 void dac_vol_to_raw_data_test(void) {
-    ASSERT_EQ(dac_vol_to_raw_data(15.0), 0x3000);
+    ASSERT_FP_EQ(dac_vol_to_raw_data(15.0), 0x3000);
 }
 
 void temp_raw_data_to_temperature_test(void) {
-    ASSERT_EQ(temp_raw_data_to_temperature(129), 1);
+    ASSERT_FP_EQ(temp_raw_data_to_temperature(129), 1);
 }
 
 void hum_raw_data_to_humidity_test(void) {
-    uint32_t raw_data = (1 << 13) - 1;
-    ASSERT_EQ(raw_data_to_humidity(raw_data), 50.0);
+    uint32_t raw_data = (1UL << 13) - 1;
+    ASSERT_FP_EQ(hum_raw_data_to_humidity(raw_data), 50.0);
 }
 
 void pres_raw_data_to_pressure_test(void) {
-    uint32_t raw_data = (1 << 16);
-    ASSERT_EQ(pres_raw_data_to_pressure(raw_data), 65.536);
+    uint32_t raw_data = (1UL << 16);
+    ASSERT_FP_EQ(pres_raw_data_to_pressure(raw_data), 65.536);
 }
 
 void opt_adc_raw_data_to_vol_test(void) {
-    uint32_t raw_data = 1 << 23;
+    uint32_t raw_data = 1UL << 23;
     uint8_t gain = 1 < 2;
-    ASSERT_EQ(opt_adc_raw_data_to_vol(raw_data, gain), 0.3125);
+    ASSERT_FP_EQ(opt_adc_raw_data_to_vol(raw_data, gain), 0.3125);
 }
 
 void therm_res_to_temp_test(void) {
     double resistance = 120.0;
     double temp = (double)(-35.0 + 5*(28.171/34.824)); //linear approximation
-    ASSERT_EQ(therm_res_to_temp(120.0), temp);
+    ASSERT_FP_EQ(therm_res_to_temp(resistance), temp);
 }
 
 void therm_temp_to_res_test(void) {
     double resistance = 120.0;
     double temp = (double)(-35.0 + 5*(28.171/34.824));
-    ASSERT_EQ(therm_temp_to_res(temp), resistance);
+    ASSERT_FP_EQ(therm_temp_to_res(temp), resistance);
 }
 
 void therm_res_to_vol_test(void) {
     double resistance = 10.0;
     double voltage = 1.25;
-    ASSERT_EQ(therm_res_to_vol(resistance), 1.25);
+    ASSERT_FP_EQ(therm_res_to_vol(resistance), voltage);
 }
 
 void therm_vol_to_res_test(void) {
     double voltage = 1.25;
     double therm_res = 10.0;
-    ASSERT_EQ(therm_vol_to_res(voltage), therm_res);
+    ASSERT_FP_EQ(therm_vol_to_res(voltage), therm_res);
 }
 
 test_t t1 = { .name = "adc_data_to_voltage", .fn = adc_data_to_voltage_test };
@@ -105,7 +106,6 @@ test_t t13 = { .name = "therm_res_to_temp", .fn = therm_res_to_temp_test };
 test_t t14 = { .name = "therm_temp_to_res", .fn = therm_temp_to_res_test };
 test_t t15 = { .name = "therm_res_to_vol", .fn = therm_res_to_vol_test };
 test_t t16 = { .name = "therm_vol_to_res", .fn = therm_vol_to_res_test };
-
 
 test_t* suite[16] = { &t1, &t2, &t3, &t4, &t5, &t6, &t7, &t8, &t9, &t10, &t11, &t12, &t13, &t14, &t15, &t16 };
 
