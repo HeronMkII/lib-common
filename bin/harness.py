ffade8f8a9721fd0a3e4a3c233a8db7bc1a12325
Brytni Richards <brytniri@gmail.com>
2019-05-15 20:51:18 -0400

    Created harness improvements branch and added automatically detect uart port

--- bin/harness.py
+++ bin/harness.py
@@ -27,17 +27,6 @@ harness_description = ("This test harness runs tests built using the libtest " +
 
 sep = 80*"-"
 
-# This function assumes an offset between the programming and uart ports of 2 for
-# mac or linux devices (posix)(i.e. uart is 2 more than programming port), or -1 for windows (nt)
-# devices. Mac USB serial ports should be of the form /dev/tty.usbmodem00xxxxxx,
-# while windows ports have the form COMx.
-def uart_offset():
-    if os.name == 'posix':
-        return 2
-    elif os.name == 'nt':
-        return -1
-
-
 class TestHarness:
     # Baud Rate specified by 32m1 data sheet
     baud_rate = 9600
@@ -426,8 +415,8 @@ if __name__ == "__main__":
     parser.add_argument('-p', '--prog', nargs='+', required=True,
             metavar=('port1', 'port2'),
             help='list of programming ports')
-    parser.add_argument('-u', '--uart', nargs='+', required=False,
-            metavar=('uart1', 'uart2'), default=[],
+    parser.add_argument('-u', '--uart', nargs='+', required=True,
+            metavar=('uart1', 'uart2'),
             help='list of UART ports')
     parser.add_argument('-d', '--test-dir', required=True,
             metavar='test_dir',
@@ -438,16 +427,6 @@ if __name__ == "__main__":
     test_path = args.test_dir
     port = args.prog
     uart = args.uart
-
-    # If there is no uart argument, add port using uart offset
-    # This is done by removing the last digit and adding uart_offset()
-    # Then, it adds it to the uart variable (list)
-    # However, this will not work for cases when port ends in 8 or 9
-    if len(uart) == 0:
-        for p in port:
-            (head, tail) = os.path.split(p)
-            tail = tail[:-1] + str(int(tail[-1]) + uart_offset())
-            uart.append(os.path.join(head, tail))
 
     # Creates TestHarness object
     harness = TestHarness(port, uart)
