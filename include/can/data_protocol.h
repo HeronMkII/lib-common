3f6412aebad0af5efadf0fc2899e4228e6b799be
Bruno Almeida <brunofalmeida@users.noreply.github.com>
2019-06-23 11:55:34 -0400

    Updated CAN data protocol with new commands

--- include/can/data_protocol.h
+++ include/can/data_protocol.h
@@ -35,34 +35,46 @@ Data formats are based on document: https://utat-ss.readthedocs.io/en/master/our
 
 // EPS housekeeping
 // 0 to 11: field number = ADC channel number
-#define CAN_EPS_HK_BB_VOL       0
-#define CAN_EPS_HK_BB_CUR       1
-#define CAN_EPS_HK_NY_CUR       2
-#define CAN_EPS_HK_PX_CUR       3
-#define CAN_EPS_HK_PY_CUR       4
-#define CAN_EPS_HK_NX_CUR       5
-#define CAN_EPS_HK_BAT_TEMP1    6
-#define CAN_EPS_HK_BAT_TEMP2    7
-#define CAN_EPS_HK_BAT_VOL      8
-#define CAN_EPS_HK_BAT_CUR      9
-#define CAN_EPS_HK_BT_CUR       10
-#define CAN_EPS_HK_BT_VOL       11
-#define CAN_EPS_HK_HEAT_SP1     12
-#define CAN_EPS_HK_HEAT_SP2     13
-#define CAN_EPS_HK_GYR_UNCAL_X  14
-#define CAN_EPS_HK_GYR_UNCAL_Y  15
-#define CAN_EPS_HK_GYR_UNCAL_Z  16
-#define CAN_EPS_HK_GYR_CAL_X    17
-#define CAN_EPS_HK_GYR_CAL_Y    18
-#define CAN_EPS_HK_GYR_CAL_Z    19
+#define CAN_EPS_HK_BB_VOL           0
+#define CAN_EPS_HK_BB_CUR           1
+#define CAN_EPS_HK_NY_CUR           2
+#define CAN_EPS_HK_PX_CUR           3
+#define CAN_EPS_HK_PY_CUR           4
+#define CAN_EPS_HK_NX_CUR           5
+#define CAN_EPS_HK_BAT_TEMP1        6
+#define CAN_EPS_HK_BAT_TEMP2        7
+#define CAN_EPS_HK_BAT_VOL          8
+#define CAN_EPS_HK_BAT_CUR          9
+#define CAN_EPS_HK_BT_CUR           10
+#define CAN_EPS_HK_BT_VOL           11
+#define CAN_EPS_HK_HEAT_SP1         12
+#define CAN_EPS_HK_HEAT_SP2         13
+#define CAN_EPS_HK_GYR_UNCAL_X      14
+#define CAN_EPS_HK_GYR_UNCAL_Y      15
+#define CAN_EPS_HK_GYR_UNCAL_Z      16
+#define CAN_EPS_HK_GYR_CAL_X        17
+#define CAN_EPS_HK_GYR_CAL_Y        18
+#define CAN_EPS_HK_GYR_CAL_Z        19
+#define CAN_EPS_HK_HEAT_SHADOW_SP1  20
+#define CAN_EPS_HK_HEAT_SHADOW_SP2  21
+#define CAN_EPS_HK_HEAT_SUN_SP1     22
+#define CAN_EPS_HK_HEAT_SUN_SP2     23
 // Number of fields
-#define CAN_EPS_HK_FIELD_COUNT  20
+#define CAN_EPS_HK_FIELD_COUNT      24
 
 // EPS control
-#define CAN_EPS_CTRL_HEAT_SP1       1
-#define CAN_EPS_CTRL_HEAT_SP2       2
-#define CAN_EPS_CTRL_RESET          3
-#define CAN_EPS_CTRL_READ_EEPROM    4
+#define CAN_EPS_CTRL_PING                   0
+#define CAN_EPS_CTRL_HEAT_SHADOW_SP1        1
+#define CAN_EPS_CTRL_HEAT_SHADOW_SP2        2
+#define CAN_EPS_CTRL_HEAT_SUN_SP1           3
+#define CAN_EPS_CTRL_HEAT_SUN_SP2           4
+#define CAN_EPS_CTRL_HEAT_CUR_THRESH_LOWER  5
+#define CAN_EPS_CTRL_HEAT_CUR_THRESH_UPPER  6
+#define CAN_EPS_CTRL_RESET                  7
+#define CAN_EPS_CTRL_READ_EEPROM            8
+#define CAN_EPS_CTRL_RESTART_COUNT          9
+#define CAN_EPS_CTRL_RESTART_REASON         10
+#define CAN_EPS_CTRL_UPTIME                 11
 
 // PAY housekeeping
 #define CAN_PAY_HK_TEMP         0
@@ -91,11 +103,13 @@ Data formats are based on document: https://utat-ss.readthedocs.io/en/master/our
 #define CAN_PAY_OPT_FIELD_COUNT 32
 
 // PAY control
+#define CAN_PAY_CTRL_PING           0
 #define CAN_PAY_CTRL_HEAT_SP1       1
 #define CAN_PAY_CTRL_HEAT_SP2       2
 #define CAN_PAY_CTRL_ACT_UP         3
 #define CAN_PAY_CTRL_ACT_DOWN       4
 #define CAN_PAY_CTRL_RESET          5
 #define CAN_PAY_CTRL_READ_EEPROM    6
+#define CAN_PAY_CTRL_ERASE_EEPROM   7
 
 #endif
